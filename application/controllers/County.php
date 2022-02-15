e3611ca65efeb89a80ce37625d8af096cb8f8bbe
Gellifree <mfw.kovacs.norbert@gmail.com>
2022-02-15 21:47:43 +0100

    feat: megye listázának jogosultsághoz (login) kötése

--- application/controllers/County.php
+++ application/controllers/County.php
@@ -18,7 +18,9 @@ class County extends CI_Controller {
     }
 
     public function list($county_id = NULL) {
-
+        if(!$this->ion_auth->logged_in()) {
+          redirect(base_url())
+        }
         if($county_id == null) {
             $view_params = [
                 'title'      => lang('county_list_title'),
