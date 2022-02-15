36e91013cf71362dc9f83e4cdd4a0d58024d3e55
Gellifree <mfw.kovacs.norbert@gmail.com>
2022-02-15 19:04:39 +0100

    feat: csoport szerkesztése nézet javítás

--- application/views/auth/edit_group.php
+++ application/views/auth/edit_group.php
@@ -1,3 +1,9 @@
+<?php $this->load->view('common/bootstrap'); ?>
+<?php $this->load->view('common/navbar'); ?>
+
+<div class="container border shadow-sm rounded bg-white mt-4">
+
+
 <h1><?php echo lang('edit_group_heading');?></h1>
 <p><?php echo lang('edit_group_subheading');?></p>
 
@@ -6,15 +12,29 @@
 <?php echo form_open(current_url());?>
 
       <p>
-            <?php echo lang('edit_group_name_label', 'group_name');?> <br />
-            <?php echo form_input($group_name);?>
+            <?php echo form_input(
+                    $group_name,
+                    '',
+                    ['placeholder' => lang('edit_group_name_label'), 'class' => 'form-control my-2']
+            ); ?>
+
       </p>
 
       <p>
-            <?php echo lang('edit_group_desc_label', 'description');?> <br />
-            <?php echo form_input($group_description);?>
+            <?php echo form_input(
+                    $group_description,
+                    '',
+                    ['placeholder' => lang('edit_group_desc_label'), 'class' => 'form-control my-2']
+            ); ?>
       </p>
 
-      <p><?php echo form_submit('submit', lang('edit_group_submit_btn'));?></p>
+      <?php echo form_submit(
+        'submit',
+        lang('edit_group_submit_btn'),
+        ['class' => 'btn btn-info']
+      );
+        ?>
 
 <?php echo form_close();?>
+
+</div>
